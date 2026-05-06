5e51a2d03f4f1c3cffc4af4ed0eca59d6b18daff
Luis <luiseulate@proton.me>
2026-05-06 11:31:59 +0200

    🔧 fix(albums): Simplify image resolution logic in getAlbumImages

--- src/lib/data-utils.ts
+++ src/lib/data-utils.ts
@@ -426,9 +426,7 @@ export async function getAlbumImages(albumId: string) {
   images = Object.fromEntries(
     Object.entries(images).filter(([key]) => key.includes(albumId)),
   )
-  const resolvedImages = await Promise.all(
+  return Promise.all(
     Object.values(images).map((image) => image().then((mod) => mod.default)),
   )
-  resolvedImages.sort(() => Math.random() - 0.5)
-  return resolvedImages
 }
